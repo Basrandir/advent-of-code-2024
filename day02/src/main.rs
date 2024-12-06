9fb7f6e6dc27d5cc8b9c465c3f587d195dc9a5a6
Bassam Saeed <github@bassamsaeed.ca>
2024-12-06 00:54:46 -0500

    Replace for loop with filter

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -8,28 +8,24 @@ fn parse_input(input: &str) -> Vec<String> {
         .collect()
 }
 
-fn part1(reports: Vec<String>) -> u32 {
-    let mut safe = 0;
-    for report in reports {
-        let levels: Vec<u32> = report
-            .split_whitespace()
-            .map(|num| num.parse::<u32>().unwrap())
-            .collect();
+fn part1(reports: Vec<String>) -> usize {
+    reports
+        .into_iter()
+        .filter(|report| {
+            let levels: Vec<u32> = report
+                .split_whitespace()
+                .map(|num| num.parse::<u32>().unwrap())
+                .collect();
 
-        if levels
-            .windows(2)
-            .all(|num| num[0] > num[1] && (num[0] - num[1] <= 3))
-        {
-            safe += 1;
-        } else if levels
-            .windows(2)
-            .all(|num| num[0] < num[1] && (num[1] - num[0] <= 3))
-        {
-            safe += 1;
-        }
-    }
-
-    safe
+            // Either descending or ascending
+            levels
+                .windows(2)
+                .all(|num| num[0] > num[1] && (num[0] - num[1] <= 3))
+                || levels
+                    .windows(2)
+                    .all(|num| num[0] < num[1] && (num[1] - num[0] <= 3))
+        })
+        .count()
 }
 
 fn main() {
